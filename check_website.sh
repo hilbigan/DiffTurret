6fb66a0b20d39a8a959f7ab7f8211f91c769e335
Aaron Hilbig <darkolan2@gmail.com>
2023-06-29 22:42:35 +0200

    Limit message size

--- check_website.sh
+++ check_website.sh
@@ -2,6 +2,8 @@
 set -euo pipefail
 cd "$(dirname $0)"
 
+MESSAGE_BYTE_LIMIT=1900
+
 # discord.env must set environment variables like so:
 # DISCORD_TOKEN="..."
 # DISCORD_CHANNEL="..."
@@ -27,7 +29,7 @@ if [ -e "$FILENAME" ]; then
     mv "$FILENAME" "$FILENAME.old"
     load_site
     set +e # allow non-zero exit codes
-    diff -u "$FILENAME" "$FILENAME.old" > /dev/null
+    diff -u -W80 "$FILENAME" "$FILENAME.old" > /dev/null
     DIFF=$?
     set -e # end
     if [ $DIFF -ne 0 ]; then
@@ -35,7 +37,7 @@ if [ -e "$FILENAME" ]; then
         echo "Site $WEBSITE has changed:" > $TMP
         echo '```diff' >> $TMP
         set +e # allow non-zero exit codes
-        diff -u "$FILENAME.old" "$FILENAME" >> $TMP
+        diff -u -W80 "$FILENAME.old" "$FILENAME" | head -c $MESSAGE_BYTE_LIMIT >> $TMP
         set -e # end
         echo '```' >> $TMP
         cat "$TMP" | ./notify.py $DISCORD_TOKEN $DISCORD_CHANNEL
