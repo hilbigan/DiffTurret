0c5dbec5bc6bff4b36443798d12bea67ad55a4fb
Aaron Hilbig <darkolan2@gmail.com>
2023-06-30 13:11:00 +0200

    Trim individual lines instead of whole message

--- check_website.sh
+++ check_website.sh
@@ -2,7 +2,8 @@
 set -euo pipefail
 cd "$(dirname $0)"
 
-MESSAGE_BYTE_LIMIT=1900
+MESSAGE_TOTAL_LENGTH_LIMIT=1900
+MESSAGE_LINE_LENGTH_LIMIT=120
 
 # discord.env must set environment variables like so:
 # DISCORD_TOKEN="..."
@@ -29,7 +30,7 @@ if [ -e "$FILENAME" ]; then
     mv "$FILENAME" "$FILENAME.old"
     load_site
     set +e # allow non-zero exit codes
-    diff -u -W80 "$FILENAME" "$FILENAME.old" > /dev/null
+    diff -u "$FILENAME" "$FILENAME.old" > /dev/null
     DIFF=$?
     set -e # end
     if [ $DIFF -ne 0 ]; then
@@ -37,7 +38,7 @@ if [ -e "$FILENAME" ]; then
         echo "Site $WEBSITE has changed:" > $TMP
         echo '```diff' >> $TMP
         set +e # allow non-zero exit codes
-        diff -u -W80 "$FILENAME.old" "$FILENAME" | head -c $MESSAGE_BYTE_LIMIT >> $TMP
+        diff -u "$FILENAME.old" "$FILENAME" | cut -c -$MESSAGE_LINE_LENGTH_LIMIT | head -c $MESSAGE_TOTAL_LENGTH_LIMIT >> $TMP
         set -e # end
         echo '```' >> $TMP
         cat "$TMP" | ./notify.py $DISCORD_TOKEN $DISCORD_CHANNEL
